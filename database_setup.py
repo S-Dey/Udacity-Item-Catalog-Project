f51c1c378c5d02a36063e3d51cbfd0c333e265c6
Subhadeep Dey <contact.sdey@gmail.com>
2018-05-18 23:28:08 +0530

    Add complete database setup

---
+++ database_setup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# Module to set up database.
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    """Class to create the table 'user'."""
+
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
+class Category(Base):
+    """Class to create the table 'category'."""
+
+    __tablename__ = "category"
+
+    id = Column(Integer, primary_key=True)
+    category = Column(String(50), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+
+        return {
+            'id': self.id,
+            'category': self.category
+        }
+
+
+class Item(Base):
+    """Class to create the table 'item'."""
+
+    __tablename__ = "item"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    description = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+        }
+
+
+engine = create_engine('sqlite:///itemcatalog.db')
+Base.metadata.create_all(engine)
